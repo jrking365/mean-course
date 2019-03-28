8c3aa4b8e2e4a95792354a8d8e245425e7361dda
Guiala Jean Roger <nigomijeanroger@gmail.com>
2019-03-28 16:04:41 -0400

    double way binding on value of textarea

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 
  export class PostCreateComponent {
+   newPost = 'NO CONTENT';
+
    onAddPost() {
      alert('Post added');
+     this.newPost = 'The user\'s post';
    }
  }
