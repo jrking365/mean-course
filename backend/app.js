d890501685410d264b957ee6eef02c5bb4933c8b
jrking365 <nigomijeanroger@gmail.com>
2019-04-01 13:57:01 -0400

    transforming response from mongodb

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,6 @@ app.use((req,res,next) => {
   next();
 });
 
-// S3lB59VEWlIcIT4F
 
 app.post("/api/posts",(req,res,next) => {
   const post = new Post({
@@ -44,22 +43,16 @@ app.post("/api/posts",(req,res,next) => {
 
 
 app.get('/api/posts',(req,res,next) => {
-  const posts = [
-    {
-      id:'fadf12421l',
-      title:'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id:'jdvsj1365f',
-      title:'Second server-side post',
-      content: 'This is coming from the server!'
-    }
-  ];
- res.status(200).json({
-   message:'Posts fetched successfully!',
-   posts : posts
- });
+  Post.find()
+  .then(documents =>{
+    res.status(200).json({
+      message:'Posts fetched successfully!',
+      posts : documents
+    });
+  }).catch(err=>{
+    console.log("an error occured");
+  });
+
 });
 
 module.exports = app;
