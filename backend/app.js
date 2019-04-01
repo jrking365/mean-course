2a39603a304506e5f1465811f124a93c545ad4cd
jrking365 <nigomijeanroger@gmail.com>
2019-04-01 10:38:05 -0400

    accessing our posts via our service from our nodejs  app

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+
+const app = express();
+
+app.use('/api/posts',(req,res,next) => {
+  const posts = [
+    {
+      id:'fadf12421l',
+      title:'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id:'jdvsj1365f',
+      title:'Second server-side post',
+      content: 'This is coming from the server!'
+    }
+  ];
+ res.status(200).json({
+   message:'Posts fetched successfully!',
+   posts : posts
+ });
+});
+
+module.exports = app;
