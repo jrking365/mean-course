bf1d0f5af28c55c7b808b24afeaacc55bd78e69b
jrking365 <nigoumijeanroger@gmail.com>
2019-04-01 07:12:30 -0400

    working with service

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { Component, } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
-import {Post} from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -10,20 +10,18 @@ import {Post} from '../post.model';
 
 })
 
- export class PostCreateComponent {
-   newPost = 'NO CONTENT';
-   enteredContent = '';
-   enteredTitle = '';
-   @Output() postCreated = new EventEmitter <Post>();
+export class PostCreateComponent {
+  newPost = 'NO CONTENT';
+  enteredContent = '';
+  enteredTitle = '';
 
-   onAddPost(form: NgForm) {
-     if (form.invalid) {
-       return;
-     }
-     const post: Post = {
-      title : form.value.title,
-      content : form.value.content
-    };
-     this.postCreated.emit(post);
-   }
- }
+
+  constructor(public postsService: PostsService) { }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+  }
+}
