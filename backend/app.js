ec93e46a409349b2a37249d892e88a1c17804653
jrking365 <nigomijeanroger@gmail.com>
2019-04-02 09:27:07 -0400

    enabling routes in the backend

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts');
+
 const app = express();
 
 mongoose.connect("mongodb+srv://jrking:S3lB59VEWlIcIT4F@cluster0-7szok.mongodb.net/node-angular?retryWrites=true",{useNewUrlParser: true})
@@ -31,62 +32,7 @@ app.use((req,res,next) => {
   next();
 });
 
+app.use('/api/posts',postsRoutes);
 
-app.post("/api/posts",(req,res,next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save()
-  .then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-});
-
-
-app.get('/api/posts',(req,res,next) => {
-  Post.find()
-  .then(documents =>{
-    res.status(200).json({
-      message:'Posts fetched successfully!',
-      posts : documents
-    });
-  }).catch(err=>{
-    console.log("an error occured");
-  });
-
-});
-
-app.put("/api/posts/:id",(req,res,next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-  Post.updateOne({_id: req.params.id},post)
-  .then((result) =>{
-    res.status(200).json({message: 'Update successful'});
-  });
-});
-app.get("/api/posts/:id",(req,res,next)=>{
-  Post.findById(req.params.id)
-  .then(post=>{
-    if(post){
-      res.status(200).json(post);
-    }else{
-      res.status(404).json({message: "Post not Found"});
-    }
-  })
-})
-
-app.delete("/api/posts/:id",(req,res,next) =>{
-  Post.deleteOne({_id: req.params.id})
-  .then(result => {
-    res.status(200).json({message:"Post deleted"})
-  });
-});
 
 module.exports = app;
