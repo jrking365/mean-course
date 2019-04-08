89d72e5546589699d488cf924ceb1982937dd62d
jrking365 <nigomijeanroger@gmail.com>
2019-04-08 10:15:41 -0400

    correction fusion

--- src/app/posts/post-create/mime-type.validator.ts
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -1,12 +1,4 @@
 import { AbstractControl } from '@angular/forms';
-<<<<<<< HEAD
-import { Observable } from 'rxjs';
-
-export const mimeType =
-(control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key: string]: any}> => {
-
-};
-=======
 import { Observable, Observer } from 'rxjs';
 
 export const mimeType = (control: AbstractControl): Promise<{ [key: string]: any }> | Observable<{ [key: string]: any }> => {
@@ -17,6 +9,5 @@ export const mimeType = (control: AbstractControl): Promise<{ [key: string]: any
 
         });
         fileReader.readAsArrayBuffer(file);
-    })
+    });
 };
->>>>>>> 15ff5fddd91515e8e19363ebeff5418edb155171
