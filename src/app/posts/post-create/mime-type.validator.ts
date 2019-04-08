6ce5e7e2cafcceef13d41db3d38d0a251a79b719
jrking365 <nigomijeanroger@gmail.com>
2019-04-08 10:11:10 -0400

    travail sur la vue

---
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -0,0 +1,7 @@
+import { AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+export const mimeType =
+(control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key: string]: any}> => {
+
+};
